1a9f44101ada201da7fcfba864385a44a3c6714e
Naman Gogia <namangogia2001@gmail.com>
2022-01-28 13:17:58 +0530

    Adding level 4 milestone: tests passing

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
 from django.urls import path
 
+from tasks.views import (
+    tasks_view,
+    completed_view,
+    add_task_view,
+    complete_task_view,
+    delete_task_view,
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    # Add all your views here
+    path("tasks/", tasks_view),
+    path("completed_tasks/", completed_view),
+    path("add-task/", add_task_view),
+    path("complete_task/<int:index>/", complete_task_view),
+    path("delete-task/<int:index>/", delete_task_view),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,1 +1,32 @@
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+
+active_tasks = []
+completed_tasks = []
+
 # Add all your views here
+def tasks_view(request):
+    return render(request, "pending_tasks.html", {"tasks": active_tasks})
+
+
+def completed_view(request):
+    return render(request, "completed_tasks.html", {"tasks": completed_tasks})
+
+
+def add_task_view(request):
+    task_to_add = request.GET.get("task")
+    active_tasks.append(task_to_add)
+    return HttpResponseRedirect("/tasks")
+
+
+def delete_task_view(request, index):
+    if index <= len(active_tasks):
+        del active_tasks[index - 1]
+    return HttpResponseRedirect("/tasks")
+
+
+def complete_task_view(request, index):
+    if index <= len(active_tasks):
+        completed_tasks.append(active_tasks[index - 1])
+        del active_tasks[index - 1]
+    return HttpResponseRedirect("/tasks")
