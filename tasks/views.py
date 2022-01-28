1654e84b2875ac4dc10661bd1379bbb75c1fb700
Naman Gogia <namangogia2001@gmail.com>
2022-01-28 16:11:12 +0530

    Adding navbar

--- tasks/views.py
+++ tasks/views.py
@@ -4,15 +4,17 @@ from django.http import HttpResponseRedirect
 active_tasks = []
 completed_tasks = []
 
-# Add all your views here
+# Pending tasks
 def tasks_view(request):
     return render(request, "pending_tasks.html", {"tasks": active_tasks})
 
 
+# Completed tasks
 def completed_view(request):
     return render(request, "completed_tasks.html", {"tasks": completed_tasks})
 
 
+# All tasks
 def all_tasks_view(request):
     return render(
         request,
@@ -21,18 +23,21 @@ def all_tasks_view(request):
     )
 
 
+# Add a task
 def add_task_view(request):
     task_to_add = request.GET.get("task")
     active_tasks.append(task_to_add)
     return HttpResponseRedirect("/tasks")
 
 
+# Delete a task
 def delete_task_view(request, index):
     if index <= len(active_tasks):
         del active_tasks[index - 1]
     return HttpResponseRedirect("/tasks")
 
 
+# Mark task as complete
 def complete_task_view(request, index):
     if index <= len(active_tasks):
         completed_tasks.append(active_tasks[index - 1])
